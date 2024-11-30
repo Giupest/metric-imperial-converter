8a513b695aeeffb4a2adba24da04fe6b21b713d2
Giupest <giucala93@gmail.com>
2024-11-30 12:04:11 +0100

    update

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,26 +1,72 @@
 function ConvertHandler() {
   
+  this.regexp = /^((\d+|\d+.\d+)(\/(\d+|\d+.\d+))?)?(l|lbs|kg|gal|km|mi){1}$/i
+  this.regexpUnit = /^([^a-zA-Z]*)?(l|lbs|kg|gal|km|mi){1}$/i
+  this.regexpNumber = /^((\d+|\d+.\d+)(\/(\d+|\d+.\d+))?)?([a-zA-Z])/
+  
+  this.spellOut = {
+    "km":"kilometers",
+    "mi":"miles",
+    "L":"liters",
+    "gal":"gallons",
+    "kg":"kilograms",
+    "lbs":"pounds"
+  }
+
   this.getNum = function(input) {
     let result;
-    
+    let qw = this.regexp.exec(input);
+    if(!this.regexp.test(input)){
+      throw new Error("error");
+
+    } else if(!/\//.test(input)){
+      if (qw[1]){
+      result = Number(qw[1]);
+      }  else {
+        result = Number(1);
+      }
+    } else {
+      result = Number(qw[2]) / Number(qw[4]);
+    }
     return result;
   };
   
   this.getUnit = function(input) {
     let result;
-    
+    let qw = this.regexp.exec(input);
+   
+    result = "";
+    if(!this.regexpUnit.test(input)){
+      throw new Error("error");
+    } else {
+    let unit = qw[qw.length-1].toLowerCase();
+    if (unit=="l"){
+      unit = unit.toUpperCase();
+    }
+      result = unit;
+    }
+
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+      switch (initUnit){
+        case "L": result= "gal";break;
+        case "gal": result= "L";break;
+        case "kg": result= "lbs";break;
+        case "lbs": result= "kg";break;
+        case "km": result= "mi";break;
+        case "mi": result= "km";break;
+        default: result = false;
+
+      }
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
-    
+      result = this.spellOut[unit];
     return result;
   };
   
@@ -29,16 +75,23 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
-    return result;
+    switch (initUnit){
+        case "L": result= initNum/galToL;break;
+        case "gal": result= initNum*galToL;break;
+        case "kg": result= initNum/lbsToKg;break;
+        case "lbs": result= initNum*lbsToKg;break;
+        case "km": result= initNum/miToKm;break;
+        case "mi": result= initNum*miToKm;break;
+        default: result = false;
+
+      }
+    return result.toFixed(5);
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return`${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
   };
-  
+   
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,31 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
+  app.route("/api/convert/").get((req,res)=>{
+  	const input = req.query.input;
+  	const reg3 = convertHandler.regexp;
+    const regUnit = convertHandler.regexpUnit;
+    const regNumber = convertHandler.regexpNumber;
+  	if(!reg3.test(input)){
+      if(!regNumber.test(input)&&!regUnit.test(input)){
+        res.send("invalid number and unit");
+      } else if (!regNumber.test(input)&&regUnit.test(input)) {
+        res.send("invalid number");
+      } else if (!regUnit.test(input)&&regNumber.test(input)){
+        res.send("invalid unit");
+      }
+  	} else {
+	  	res.json({
+	  		"initNum":convertHandler.getNum(input),
+	  		"initUnit":convertHandler.getUnit(input),
+	  		"returnNum":Number(convertHandler.convert(convertHandler.getNum(input),convertHandler.getUnit(input))),
+	  		"returnUnit":convertHandler.getReturnUnit(convertHandler.getUnit(input)),
+	  		"string": convertHandler.getString(convertHandler.getNum(input),
+                  convertHandler.spellOutUnit(convertHandler.getUnit(input)),
+                  Number(convertHandler.convert(convertHandler.getNum(input),
+                  convertHandler.getUnit(input))),
+                  convertHandler.spellOutUnit(convertHandler.getReturnUnit(convertHandler.getUnit(input)))),
+	 	});
+  	}
+  });
 };
